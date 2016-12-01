2fc99b06dce88c9c8a5dbbee3c837ffae75e21c1
wangnanphp <wangnanphp@163.com>
2016-12-02 01:45:36 +0800

    完善代码

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -3,6 +3,12 @@
 /**
  * Class MY_Controller 应用的基控制器
  *
+ * @property CI_Config                     $config
+ * @property CI_Form_validation            $form_validation
+ * @property CI_Input                      $input
+ * @property CI_Pagination                 $pagination
+ * @property CI_Session                    $session
+ *
  * @author wangnan <wangnanphp@163.com>
  * @date   2016-12-01 12:16:50
  */
